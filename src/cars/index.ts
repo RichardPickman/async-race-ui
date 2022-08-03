91cfcbd3399ca3a3ce8e856d3428f109211b3d18
RichardPickman <drezzerock@gmail.com>
2022-08-03 21:44:39 +0200

    feat: add dumb car db

---
+++ src/cars/index.ts
@@ -0,0 +1,25 @@
+export const company = [
+    "Ford",
+    "Chevrolet",
+    "Porsche",
+    "Aston Martin",
+    "Mercedes",
+    "BMW",
+    "Toyota",
+    "Opel",
+    "Rolls-Royce",
+    "Nissan",
+];
+
+export const model = [
+    "Mustang",
+    "Camaro",
+    "Cayenne",
+    "Ulster",
+    "C-class",
+    "3 series",
+    "Camry",
+    "Astra",
+    "Phantom",
+    "Z",
+];
